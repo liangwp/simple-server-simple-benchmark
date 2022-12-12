dc0df1064a539cf6eb2c6163f8ac71c55e375cbb
weipang <liangwp@gmail.com>
2022-12-12 09:10:32 +0800

    increase number of requests to servers, perform requests in a single call to curl, instead of calling curl in a loop

--- curl_loop.sh
+++ curl_loop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for i in {1..100}; do
-  curl -s 'localhost:8000' > /dev/null
-done
+array=();
+for i in {1..1000}; do
+  array+=(--next http://localhost:8000 ) ; 
+done;
+
+time {
+  curl -s "${array[@]}" > /dev/null;
+}
